ad45597cb3f7d0bfbb4f0d46b069a2991d1a3e6e
Reza Karimi <mr.rezakarimi99@gmail.com>
2025-01-26 11:11:12 +0330

    Change approach

--- src/LicenseChecker.php
+++ src/LicenseChecker.php
@@ -6,14 +6,22 @@ use Illuminate\Support\Facades\Http;
 
 class LicenseChecker
 {
+    /**
+     * @throws \Exception
+     */
     public function isLicenseValid(): bool
     {
         $licenseKey = config('license.license_key');
         $domain = config('app.url');
 
         if (empty($licenseKey)) {
-            return false;
+            throw new \Exception('License key is not set.');
         }
+
+        if (empty($domain)) {
+            throw new \Exception('Domain is not set.');
+        }
+
         $response = Http::post(config('license.server_url'), [
             'license_key' => $licenseKey,
             'domain' => $domain,
--- src/LicenseManagerServiceProvider.php
+++ src/LicenseManagerServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class LicenseManagerServiceProvider extends ServiceProvider
 {
+
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/license.php', 'license');
@@ -17,13 +18,10 @@ class LicenseManagerServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->app['events']->listen('Illuminate\Console\Events\CommandStarting', function ($event) {
-            $licenseChecker = app(LicenseChecker::class);
-
-            if (!$licenseChecker->isLicenseValid()) {
-                echo "License invalid or expired. Command execution stopped.\n";
-                exit(1);
-            }
-        });
+        $licenseChecker = app(LicenseChecker::class);
+        if (!$licenseChecker->isLicenseValid()) {
+            echo "License invalid or expired. Command execution stopped.\n";
+            exit(1);
+        }
     }
 }
